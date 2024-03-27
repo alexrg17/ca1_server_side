067bf6ab8afb7fac8a4f1470a6b878bd81fe17e6
alexrg17 <alexradugabriel7@gmail.com>
2024-03-27 18:54:34 +0000

    git 16 I am trying to style the create article page

--- resources/views/blog/Article.blade.php
+++ resources/views/blog/Article.blade.php
@@ -1,17 +1,15 @@
-@push('styles')
-<link href="{{ asset('css/home.css') }}" rel="stylesheet">
-@endpush
-
-<form class="custom-form " method="POST" action="{{ route('articles.store') }}">
-    @csrf
-    <div class="mb-4">
-        <label for="title" class="block text-gray-700 text-sm font-bold mb-2">Title</label>
-        <input type="text" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="title" name="title" value="{{ old('title') }}" required>
-    </div>
-    <div class="mb-4">
-        <label for="content" class="block text-gray-700 text-sm font-bold mb-2">Content</label>
-        <textarea class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="content" name="content" rows="6" required>{{ old('content') }}</textarea>
-    </div>
-    <input type="hidden" name="post_id" value="{{ session('GBvariable') }}">
-    <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Submit</button>
-</form>
+<div class="w-4/5 m-auto bg-yellow-300 rounded-lg shadow-md p-8">
+    <form method="POST" action="{{ route('articles.store') }}">
+        @csrf
+        <div class="mb-4">
+            <label for="title" class="block text-gray-700 text-lg font-bold mb-2">Title</label>
+            <input type="text" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="title" name="title" value="{{ old('title') }}" required>
+        </div>
+        <div class="mb-8">
+            <label for="content" class="block text-gray-700 text-lg font-bold mb-2">Content</label>
+            <textarea class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="content" name="content" rows="6" required>{{ old('content') }}</textarea>
+        </div>
+        <input type="hidden" name="post_id" value="{{ session('GBvariable') }}">
+        <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded focus:outline-none focus:shadow-outline">Submit</button>
+    </form>
+</div>
