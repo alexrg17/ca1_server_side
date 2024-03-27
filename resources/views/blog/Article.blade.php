c1b5ad0e226cde06359f08e1e353f316367325ba
alexrg17 <alexradugabriel7@gmail.com>
2024-03-27 18:56:14 +0000

    git 17 applying changes to the create article page

--- resources/views/blog/Article.blade.php
+++ resources/views/blog/Article.blade.php
@@ -3,13 +3,13 @@
         @csrf
         <div class="mb-4">
             <label for="title" class="block text-gray-700 text-lg font-bold mb-2">Title</label>
-            <input type="text" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="title" name="title" value="{{ old('title') }}" required>
+            <input type="text" style="border: 1px solid #CBD5E0; border-radius: 0.375rem;" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="title" name="title" value="{{ old('title') }}" required>
         </div>
         <div class="mb-8">
             <label for="content" class="block text-gray-700 text-lg font-bold mb-2">Content</label>
-            <textarea class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="content" name="content" rows="6" required>{{ old('content') }}</textarea>
+            <textarea style="border: 1px solid #CBD5E0; border-radius: 0.375rem;" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="content" name="content" rows="6" required>{{ old('content') }}</textarea>
         </div>
         <input type="hidden" name="post_id" value="{{ session('GBvariable') }}">
-        <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded focus:outline-none focus:shadow-outline">Submit</button>
+        <button type="submit" style="background-color: #3F83F8; border: none; border-radius: 0.375rem; color: white; cursor: pointer; padding: 0.5rem 1rem;" class="font-bold py-2 px-6 rounded focus:outline-none focus:shadow-outline">Submit</button>
     </form>
 </div>
